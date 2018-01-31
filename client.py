f7ae08629fae6d3d10f4dbb8e7ddeee6a85b99d1
GirGir <36016668+GirGir@users.noreply.github.com>
2018-01-31 17:50:39 -0600

    Bot right here

---
+++ client.py
@@ -0,0 +1,26 @@
+#Anything not working? Report it to Issues and I will help you
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+client = discord.Client()
+bot_prefix= "bot_prefix_here"
+Client = commands.Bot(command_prefix=bot_prefix)
+
+@client.event
+async def on_ready():
+print Github https://github.com/GirGir/Client-Discord.py-Bot
+
+@client.event
+async def on_message(message):
+    if message.content == "M":
+        await client.send_message(message.channel, "You are getting real dirty")
+    print ("Dirty people pervs")
+    if message.content == "Bad-stuff":
+        await client.send_message(message.channel, ":middle_finger:")
+        print ("Bad-stuff")
+    if message.content == "Raid-this_server":
+        await client.send_message(message.channel, "Spam")
+    if message.content == "Spam":
+        await client.send_message(message.channel, "Spam")
+client.run("token")
